96bbe6b358a8ac38e43d21d89786af2eb2277bd0
ylaper <zxc734@gmail.com>
2022-10-16 14:24:42 +0700

    Navbar compoment

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,10 +1,15 @@
 <template>
-  <p>Chatroom</p>
+  <div class="container">
+    <Navbar/>
+  </div>
 </template>
 
 <script>
+import Navbar from '../components/Navbar.vue'
 export default {
-
+  components: {
+    Navbar
+  }
 }
 </script>
 
