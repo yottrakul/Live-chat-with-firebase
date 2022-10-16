01dbbf48b20b47fe23fb738300af7e61350dcfda
ylaper <zxc734@gmail.com>
2022-10-16 14:32:14 +0700

    Logout user composible

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,13 +4,25 @@
       <p>Hey they... display name here</p>
       <p class="email">Currently logged in as... email</p>
     </div>
-    <button>Logout</button>
+    <button @click="handleClick">Logout</button>
   </nav>
 </template>
 
 <script>
+import useLogout from '../composables/useLogout'
 export default {
+  setup() {
+    const {logout,error} = useLogout();
 
+    const handleClick = async () => {
+      await logout()
+      if(!error.value) {
+        console.log('user logout!')
+      }
+    }
+
+    return {handleClick}
+  }
 }
 </script>
 
