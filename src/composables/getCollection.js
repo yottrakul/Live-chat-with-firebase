672632624e70a891b5c529fcda5dc62a01f7b501
ylaper <zxc734@gmail.com>
2022-10-23 02:41:03 +0700

    watchEffect onInvalidate

--- src/composables/getCollection.js
+++ src/composables/getCollection.js
@@ -1,5 +1,5 @@
 import { projectFirestore } from "@/firebase/config";
-import { ref } from "vue";
+import { ref, watchEffect } from "vue";
 
 const getCollection = (collection) => {
   const documents = ref(null);
@@ -8,7 +8,7 @@ const getCollection = (collection) => {
   let collectionRef = projectFirestore.collection(collection)
     .orderBy('createdAt', 'asc');
   
-  collectionRef.onSnapshot(snap => {
+  const unsub = collectionRef.onSnapshot(snap => {
     let results = [];
     snap.docs.forEach(doc => {
       //มีการเช็คค่า createdAt ก่อนเพิ่มรันคำสั่งต่อไป
@@ -20,6 +20,11 @@ const getCollection = (collection) => {
     console.log(err.message);
     documents.value = null;
     error.value = 'could not fetch data';
+  })
+
+  watchEffect((onInvalidate) => {
+    //unsub  จาก collection ก่อนหน้า เมื่อ watcher หยุดไปแล้ว (component unMounted)
+    onInvalidate(() => unsub())
   })
 
   return { documents, error }
